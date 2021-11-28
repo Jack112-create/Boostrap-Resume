fa0b377375fae72bc143532ffa233b31d49bf06b
Jack Conroy <jackconroy777@gmail.com>
2021-11-28 13:34:41 +0000

    Add message for too many api requests

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -2,29 +2,28 @@ function userInformationHTML(user) {
     return `
         <h2>${user.name}
             <span class="small-name">
-                (@<a href="${user.html_url}" target="_blank")>${user.login}</a>)
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
             </span>
         </h2>
         <div class="gh-content">
             <div class="gh-avatar">
                 <a href="${user.html_url}" target="_blank">
-                    <img src='${user.avatar_url}' width='80' height='80' alt='${user.login}'/>
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
                 </a>
             </div>
             <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
-        </div>
-    `
+        </div>`;
 }
 
 function repoInformationHTML(repos) {
-    if(repos.length == 0) {
+    if (repos.length == 0) {
         return `<div class="clearfix repo-list">No repos!</div>`;
     }
 
     var listItemsHTML = repos.map(function(repo) {
         return `<li>
                     <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-                </li>`
+                </li>`;
     });
 
     return `<div class="clearfix repo-list">
@@ -34,24 +33,23 @@ function repoInformationHTML(repos) {
                 <ul>
                     ${listItemsHTML.join("\n")}
                 </ul>
-            </div>`
+            </div>`;
 }
 
+function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
-function fetchGitHubInformation(e) {
-    $('#gh-user-data').html("");
-    $('#gh-repo-data').html("");
-    var username = $('#gh-username').val();
+    var username = $("#gh-username").val();
     if (!username) {
-        $('#gh-user-data').html(`<h2>Please enter a GitHub username</h2>`);
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
 
-    $('#gh-user-data').html(
+    $("#gh-user-data").html(
         `<div id="loader">
-            <img src="assets/css/loader.gif" alt="loading..."/>
-        </div>`
-    )
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`);
 
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`),
@@ -67,6 +65,9 @@ function fetchGitHubInformation(e) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
